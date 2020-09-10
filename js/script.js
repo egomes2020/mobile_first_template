f051c79bf8e5bbe5230ab24da3dced07a63f6c68
Edgar Gomes <egomes2013@outlook.com>
2020-09-10 16:58:23 -0300

    update

--- js/script.js
+++ js/script.js
@@ -7,7 +7,16 @@ const input = document.querySelector('#add-input')
 //classes names///////////////////////////////////////////
 const CHECK = "far fa-check-circle"
 const UNCHECK = "far fa-circle"
-const LINE_THROUGH = "line_through"
+const LINE_THROUGH = "lineThrough"
+
+
+//variables ///////////////////////////////////////////////////
+let LIST = [], id = 0;
+
+
+
+
+
 
 //show date////////////////////////////////////////////////
 const options = {
@@ -22,17 +31,28 @@ date.innerText = today.toLocaleDateString("br", options);
 
 
 
+
+
+
 //function add item //////////////////////////////////////////
-function addItem(item){
-    let position = "beforebegin"
-    let itemElement = `<li class="list-item"  contenteditable="true">
-                            <i class="far fa-circle" job="complete"></i>
-                            <p> ${item}<i class="fas fa-trash-alt de" job="delete"></i></p><i>
+function addItem(item, id, done, trash){
+
+    if(trash) {return;}
+
+    const DONE = done ? CHECK : UNCHECK;
+    const LINE = done ? LINE_THROUGH : "";
+
+    const position = "beforebegin"
+    const itemElement = `<li class="list-item">
+                            <i class="far ${DONE}" job="complete" ${id}></i>
+                            <p class=" text ${LINE}"contenteditable="true" > ${item}</p><i class="fas fa-trash-alt de" job="delete" ${id}></i>
                         </li>`
 
     list.insertAdjacentHTML(position, itemElement)
 
 }
+
+
 
 // add item with keyup ""enter //////////////////////////////
 document.addEventListener("keyup", function(even){
@@ -40,9 +60,53 @@ document.addEventListener("keyup", function(even){
         item=input.value
         //if the input isn´t empty
         if(item){
-            addItem(item)
+            addItem(item, id, false, false)
+
+            LIST.push({
+                name: item,
+                id: id,
+                done: false,
+                trash: false
+            })
+
+            id++
         }
 
         input.value=""
     }
+})
+
+
+
+
+//function complete item /////////////////////////////////////////
+function completeItem(element){
+    element.classList.toggle(CHECK);
+    element.classList.toggle(UNCHECK);
+    element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+
+    LIST[element.id].done = LIST[element.id].done ? false : true;
+
+}
+
+
+// function remove item ////////////////////////////////////////////////
+function removeItem (element){
+    element.parentNode.parentNode.removeChild(element.parentNode);
+
+    LIST[element.id].trash = true;
+}
+
+
+// target the items ///////////////////////////////////////////////////////
+list.addEventListener("click",(event) => {
+    const element = event.target; // returns any item clicked 
+    const elementJob = element.attributes.job.value; // complete or delete
+
+    if (elementJob == "complete"){
+            completeItem(element)
+     } else if (elementJob == "delete") {
+             removeItem(element)
+    }
+
 })
